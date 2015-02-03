c1c7961832473e67297977734140c52437a15174
Philipp Unger <unger@pluspol.info>
2015-02-03 12:01:16 +0100

    Test multi Share 2

--- _news/share.php
+++ _news/share.php
@@ -8,10 +8,9 @@ $description 	= $_GET['description'];
 <html>
     <head>
 		<meta property="fb:app_id" content="710765662369464" />
-		<meta property="og:site_name" content="Facebook Developers" />
+		<meta property="og:site_name" content="Café Wagner" />
 		<meta property="og:title" content="<?php echo $title; ?>" />
 		<meta property="og:type" content="article" />
-		<meta property="og:url" content="https://developers.facebook.com/docs/plugins" />
 		<meta property="og:image" content="<?php echo $img; ?>" />
 		<meta property="og:locale" content="de_DE" />
 		<meta property="og:description" content="<?php echo $description; ?>" />
